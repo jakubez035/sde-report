b89e0cbf2654e888ff8371f46644f912f53b1472
SashkisaHZ <atan0007@hz.nl>
2026-01-09 03:32:04 +0100

    finished the LightOn + LightOff + CommandInvoker

--- src/main/java/com/smarthome/command/CommandInvoker.java
+++ src/main/java/com/smarthome/command/CommandInvoker.java
@@ -6,8 +6,25 @@ import java.util.Stack;
  * Invoker that executes commands and supports undo functionality.
  */
 public class CommandInvoker {
-    private Stack<Command> commandHistory;
+    private Stack<Command> commandHistory = new Stack<>();
     
-    // Implementation will be added later
+    public void executeCommans(Command command) {
+        command.execute();
+        commandHistory.push(command);
+    }
+
+    public void undoLastCommand() {
+        if (!commandHistory.isEmpty()) {
+            Command lastCommand = commandHistory.pop();
+            lastCommand.undo();
+        } else {
+            System.out.println("No commands to undo.");
+        }
+    }
+
+    public void clearHistory() {
+        commandHistory.clear();
+    }
+
 }
 
