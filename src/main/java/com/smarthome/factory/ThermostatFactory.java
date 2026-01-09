48fc583fbf8c8d785ad40afee667003742c74b0f
Jakub <holikj1@gmail.com>
2026-01-09 12:03:31 +0100

    add thermostatat factory for creating thermostat instances

--- src/main/java/com/smarthome/factory/ThermostatFactory.java
+++ src/main/java/com/smarthome/factory/ThermostatFactory.java
@@ -5,12 +5,23 @@ import com.smarthome.devices.Thermostat;
 
 /**
  * Factory for creating Thermostat devices.
+ * Demonstrates Factory Method pattern.
  */
 public class ThermostatFactory implements DeviceFactory {
     @Override
     public Device createDevice(String id, String name) {
-        // Implementation will be added later
-        return null;
+        return new Thermostat(id, name);
+    }
+
+    /**
+     * Creates a thermostat with a specific initial temperature.
+     * @param id Unique identifier
+     * @param name Display name
+     * @param initialTemperature Initial temperature setting
+     * @return New Thermostat instance
+     */
+    public Device createDevice(String id, String name, double initialTemperature) {
+        return new Thermostat(id, name, initialTemperature);
     }
 }
 
