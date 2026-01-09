c071089b97562f32b764e41e5501521e020f4324
Jakub <holikj1@gmail.com>
2026-01-09 12:10:20 +0100

    add SetTempCommand with undo functionality

--- src/main/java/com/smarthome/command/SetTempCommand.java
+++ src/main/java/com/smarthome/command/SetTempCommand.java
@@ -4,18 +4,61 @@ import com.smarthome.devices.Thermostat;
 
 /**
  * Command to set thermostat temperature.
+ * Demonstrates Command pattern with undo functionality.
  */
 public class SetTempCommand implements Command {
     private Thermostat thermostat;
     private double targetTemperature;
     private double previousTemperature;
-    
-    // Implementation will be added later
-    
+    private boolean executed = false;
+
+    /**
+     * Constructor for SetTempCommand.
+     * @param thermostat The thermostat to control
+     * @param targetTemperature The target temperature to set
+     */
+    public SetTempCommand(Thermostat thermostat, double targetTemperature) {
+        this.thermostat = thermostat;
+        this.targetTemperature = targetTemperature;
+    }
+
     @Override
-    public void execute() {}
-    
+    public void execute() {
+        if (!executed) {
+            previousTemperature = thermostat.getTemperature();
+            thermostat.setTemperature(targetTemperature);
+            executed = true;
+            System.out.println("Command executed: Set temperature to " + targetTemperature + "°C");
+        } else {
+            System.out.println("Command already executed");
+        }
+    }
+
     @Override
-    public void undo() {}
+    public void undo() {
+        if (executed) {
+            thermostat.setTemperature(previousTemperature);
+            executed = false;
+            System.out.println("Command undone: Temperature restored to " + previousTemperature + "°C");
+        } else {
+            System.out.println("Nothing to undo - command not executed yet");
+        }
+    }
+
+    /**
+     * Gets the target temperature for this command.
+     * @return Target temperature
+     */
+    public double getTargetTemperature() {
+        return targetTemperature;
+    }
+
+    /**
+     * Gets the previous temperature before command execution.
+     * @return Previous temperature
+     */
+    public double getPreviousTemperature() {
+        return previousTemperature;
+    }
 }
 
